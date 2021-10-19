8b00cc649f9f8d8be0ae53544521548cea194a17
Pierrick Rancoeur <pierre.ankh@gmail.com>
2021-10-19 11:23:10 +0200

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f16f1aab6462f47fe8394bfb12274635'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bc7ed0fa-e9d5-46bd-9fb3-7cc83b8119f2'
+};
